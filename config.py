c85624dcdbbe2dc1a10ad90a4342ef8bb7b5c732
shoucf <shouchaofan@shphschool.com>
2017-08-15 19:32:26 -0700

    Update config.py

--- config.py
+++ config.py
@@ -12,17 +12,17 @@ Solenoid12Num       = 1
 Solenoid21Num       = 2
 Solenoid22Num       = 3
 Solenoid31Num       = 4
-Solenoid32Num		= 5
+Solenoid32Num		    = 5
 #Climbing Setting
 ClimbEff            = 0.6 
 ClimbMotor          = 10
 #auto setting
-Stage1				= 4
-Stage2				= 0.7
-Stage3				= 3
+Stage1				      = 4
+Stage2				      = 0.7
+Stage3			      	= 3
 
 
-lf_motor_inverse	= True 
-rf_motor_inverse	= True 
+lf_motor_inverse	  = True 
+rf_motor_inverse	  = True 
 
-pct 				= 8/100
+pct 				        = 8/100
